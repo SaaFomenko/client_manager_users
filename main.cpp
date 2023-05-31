5229b57e7ec7c5bcf0f8a77b3e9feea8bd74553f
SaaFomenko <s.fomenko@list.ru>
2023-05-31 20:06:05 +0300

    Need test new funtional in my_file library.

--- main.cpp
+++ main.cpp
@@ -2,57 +2,8 @@
 #include <exception>
 #include <fstream>
 #include <pqxx/pqxx>
+#include "lib/my_file/my_file.h"
 
-
-static const char* err_overload_size_file = "This application limit 256 chars in file.";
-static const int data_chars_in_file = 256;
-
-class MyException : public std::exception
-{
-	private:
-		const char* msg;
-
-	public:
-		MyException(const char* msg_) : msg(msg_){}
-		virtual ~MyException(){}
-
-		const char* what() const noexcept override
-		{
-			return msg;
-		}
-};
-
-const char* file_to_chars(const char* path, char devider)
-{
-	std::cout << path << " with devider: \"" << devider << "\"\n";
-	std::ifstream fin(path);
-	if (fin.fail())
-	{
-		throw MyException(err_open_file_msg);
-	}
-	
-	char symvol;
-	char data[data_chars_in_file];
-	int i = 0;
-
-	while (fin.get(symvol))
-	{
-		if (i >= data_chars_in_file)
-		{
-			throw MyException(err_overload_size_file);
-		}
-		if (symvol == '\n')
-		{
-			data[i] = devider;
-		}
-		else
-		{
-			strcat(data, symvol);
-		}
-	}
-
-	return static_cast<const char*>(data);
-} 
 
 int main()
 {
@@ -63,21 +14,19 @@ int main()
 //	std::string sql_str = "";
 //	const std::string sql_str;
 	const char* connect_str;
-	bool isConnect = false;
 
 	try
 	{
-		connect_str = file_to_chars(path_connect, " ");
-		isConnect = true;
+		MyFile file_read(path_connect);
+		connect_str = file_read.file_to_chars(" <test> ");
 	}
-	catch(std::exception e)
+	catch(std::exception& e)
 	{
 		std::cout << e.what() << path_connect << '\n';
 		std::cout << err_connect_file << '\n';
 		std::cout << quest_create_file;		
 	}
 
-//		sql_str = file_to_string(path_create_table);
 	try
 	{
 		pqxx::connection c(connect_str);
