774f08dba1f9c42c0620d822487b3baf1e862f02
SaaFomenko <s.fomenko@list.ru>
2023-11-27 11:41:47 +0300

    Status point.

--- lib/my_user/my_user.cpp
+++ lib/my_user/my_user.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "../my_file/my_file.h"
 #include "my_user.h"
 
@@ -6,5 +7,8 @@ static const char* my_profile = "profile.txt";
 
 my::User::User()
 {
-    
+    std::string str;
+
+	MyFile file_read(my_profile);
+	str = file_read.to_str();
 }
